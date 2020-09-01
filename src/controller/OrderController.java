a04df1f50256c1c242771073a547d2b47d1b1b5b
NadeeshWijesingha <nadeeshwijesingha@gmail.com>
2020-09-01 14:21:57 +0530

    Create methods on OrderController

--- src/controller/OrderController.java
+++ src/controller/OrderController.java
@@ -1,12 +1,8 @@
 package controller;
 
-import java.io.IOException;
-import java.net.URL;
-
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
-
 import dao.DAOFactory;
 import dao.DAOType;
 import dao.custom.ItemDAO;
@@ -23,61 +19,81 @@ import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+import util.ItemTM;
 import util.OrderTM;
+
+import java.io.IOException;
+import java.net.URL;
 
 public class OrderController {
 
-  public AnchorPane root;
-  public TableView <OrderTM> tblOrder;
-  public TableColumn clmOrderId;
-  public TableColumn clmItemId;
-  public TableColumn clmDescription;
-  public TableColumn clmAddQty;
-  public TableColumn clmTotal;
-  public Label lblSubTotal;
-  public JFXTextField txtId;
-  public JFXTextField txtDate;
-  public JFXTextField txtDescription;
-  public JFXTextField txtQtyOnHand;
-  public JFXTextField txtUnitPrice;
-  public JFXTextField txtAddQty;
-  public JFXComboBox comboCustomer;
-  public JFXComboBox comboItem;
-  public JFXButton btnSave;
+    public AnchorPane root;
+    public TableView<OrderTM> tblOrder;
+    public TableColumn clmOrderId;
+    public TableColumn clmItemId;
+    public TableColumn clmDescription;
+    public TableColumn clmAddQty;
+    public TableColumn clmTotal;
+    public Label lblSubTotal;
+    public JFXTextField txtId;
+    public JFXTextField txtDate;
+    public JFXTextField txtDescription;
+    public JFXTextField txtQtyOnHand;
+    public JFXTextField txtUnitPrice;
+    public JFXTextField txtAddQty;
+    public JFXComboBox comboCustomer;
+    public JFXComboBox comboItem;
+    public JFXButton btnSave;
 
-  OrdersDAO ordersDAO = DAOFactory.getInstance().getDAO(DAOType.ORDERS);
-  OrderDetailDAO orderDetailDAO = DAOFactory.getInstance().getDAO(DAOType.ORDER_DETAIL);
-  ItemDAO itemDAO =DAOFactory.getInstance().getDAO(DAOType.ITEM);
+    OrdersDAO ordersDAO = DAOFactory.getInstance().getDAO(DAOType.ORDERS);
+    OrderDetailDAO orderDetailDAO = DAOFactory.getInstance().getDAO(DAOType.ORDER_DETAIL);
+    ItemDAO itemDAO = DAOFactory.getInstance().getDAO(DAOType.ITEM);
 
-  public void initialize(){
+    public void initialize() {
 
-    tblOrder.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("itemId"));
-    tblOrder.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("description"));
-    tblOrder.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("qty"));
-    tblOrder.getColumns().get(3).setCellValueFactory(new PropertyValueFactory<>("unitPrice"));
-    tblOrder.getColumns().get(4).setCellValueFactory(new PropertyValueFactory<>("total"));
+        tblOrder.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("itemId"));
+        tblOrder.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("description"));
+        tblOrder.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("qty"));
+        tblOrder.getColumns().get(3).setCellValueFactory(new PropertyValueFactory<>("unitPrice"));
+        tblOrder.getColumns().get(4).setCellValueFactory(new PropertyValueFactory<>("total"));
 
-  }
+    }
 
-  public void btnNew_OnAction(ActionEvent actionEvent) {
-  }
+    public void btnSave_OnAction(ActionEvent actionEvent) {
+    }
 
-  public void btnSave_OnAction(ActionEvent actionEvent) {
-  }
+    public void btnDelete_OnAction(ActionEvent actionEvent) {
+    }
 
-  public void btnDelete_OnAction(ActionEvent actionEvent) {
-  }
+    public void btnPlaceOrder_OnAction(ActionEvent actionEvent) {
+    }
 
-  public void btnPlaceOrder_OnAction(ActionEvent actionEvent) {
-  }
+    public void loadAllCustomer() {
+    }
 
-  public void btnHome_OnAction(MouseEvent mouseEvent) throws IOException {
-    URL resource;
-    resource = this.getClass().getResource("/view/MainForm.fxml");
-    Parent root = FXMLLoader.load(resource);
-    Scene scene = new Scene(root);
-    Stage primaryStage = (Stage) (this.root.getScene().getWindow());
-    primaryStage.setScene(scene);
-    primaryStage.centerOnScreen();
-  }
+    public void loadAllItems() {
+    }
+
+    public void generateOrderId() {
+
+    }
+
+    public void calculateTotal() {
+
+    }
+
+    public void calculateQtyOnHand(ItemTM item) {
+
+    }
+
+    public void btnHome_OnAction(MouseEvent mouseEvent) throws IOException {
+        URL resource;
+        resource = this.getClass().getResource("/view/MainForm.fxml");
+        Parent root = FXMLLoader.load(resource);
+        Scene scene = new Scene(root);
+        Stage primaryStage = (Stage) (this.root.getScene().getWindow());
+        primaryStage.setScene(scene);
+        primaryStage.centerOnScreen();
+    }
+
 }
